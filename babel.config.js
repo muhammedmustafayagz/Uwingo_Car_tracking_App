8cfeff9fe848d6588f53214868ec021ecac3e5ab
ubeysaab <aboosh.sa262000@gmail.com>
2025-12-15 17:57:43 +0300

    chore(dep) : adding react-native-paper , edit(babel) : in order to optimize size bundle

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
 
-
+    'react-native-paper/babel',
     '@babel/plugin-transform-export-namespace-from',
     'react-native-worklets/plugin',
   ],
