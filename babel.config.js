4019791f85ce6109a4cc2fa02fdf44eaeab2bf13
ubeysaab <aboosh.sa262000@gmail.com>
2025-12-19 14:48:55 +0300

    Build(integrate babel-plugin-module-resolver) : add necessary configs in order to use path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json', '.jsx'],
+        alias: {
+          '@': './src',
+          '/assets': "./assets"
+        },
+      }
+    ],
 
     'react-native-paper/babel',
     '@babel/plugin-transform-export-namespace-from',
